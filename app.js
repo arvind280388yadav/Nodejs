dfe69c4f6bcbcd2680b99ea1a16d6ef744aca23d
arvind280388yadav <yadav2mca@gmail.com>
2024-08-18 16:11:18 +0530

    cdd

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5240;
+
+app.get('/', function (req, res) {
+    res.send('<center><h3>My First Node APP</h3></center>')
+})
+
+app.listen(PORT, function () {
+    console.log(`listening on ${PORT}`)
+})
